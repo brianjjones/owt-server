65d1145de535c31f5981da55babd48f4bae17fa5
Jones, Brian J <brian.j.jones@intel.com>
2020-02-13 15:59:58 -0800

    Fixing pack.js

--- scripts/pack.js
+++ scripts/pack.js
@@ -33,7 +33,7 @@ optParser.addOption('h', 'help', 'boolean', 'Show help');
 
 const options = optParser.parseArgs(process.argv);
 console.log('BJONES options are ');
-let keys = Object.keys(d);
+let keys = Object.keys(options);
 for (var i=0; i < keys.length; i++) {
   console.log(keys[i] + " == " + options[keys[i]] + " ## BJONES");
 }
